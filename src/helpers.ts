b113d135a2a78b341dae09de72236a3d8220d629
Hassin-bono <bloodygamer3212@gmail.com>
2026-08-01 01:32:05 +0500

    Confidence Calibration

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,3 +3,7 @@ import { APP_NAME } from "./constants";
 export function formatTitle(title: string): string {
   return `[${APP_NAME}] ${title}`;
 }
+
+export function safeFormat(val?: string) {
+  return val ? val.trim() : null;
+}
