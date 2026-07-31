6ccda09fc05cdffd2dd020d035ca2997eafaae99
Hassin-bono <bloodygamer3212@gmail.com>
2026-08-01 01:29:57 +0500

    Hardcoded Secret Detection

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,5 @@ export const API_VERSION = "v1";
 export const DEFAULT_PAGE_SIZE = 20;
 export const MAX_RETRY_COUNT = 3;
 export const TRANSITIVE_FLAG = true;
+export const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE_SECRET_12345";
 export * from "./db"
