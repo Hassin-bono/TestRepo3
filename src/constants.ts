506e98505cf38ef4d1ac4f76776ee575919dcd3c
Hassin-bono <bloodygamer3212@gmail.com>
2026-07-31 23:01:40 +0500

    Update constants.tsDependency Graph Depth Calculation

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ export const APP_NAME = "TS-Benchmark-App";
 export const API_VERSION = "v1";
 export const DEFAULT_PAGE_SIZE = 20;
 export const MAX_RETRY_COUNT = 3;
+export const TRANSITIVE_FLAG = true;
 export * from "./db"
